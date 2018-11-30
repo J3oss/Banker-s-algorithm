3f6b8a1b7e8cafd296b5a342b2a1310b3ef16dfc
Youssef Assem <34811325+J3ossOfWar@users.noreply.github.com>
2018-11-30 02:01:21 +0200

    PHASE2
    completing all data intialization

---
+++ Banker's algorithm/generator.cpp
@@ -0,0 +1,66 @@
+#include "iostream"
+
+int** allocation_generation(int no_of_resources, int no_of_processes)
+{
+	int** a = new int*[no_of_processes];
+	for (int i = 0; i < no_of_processes; ++i)
+		a[i] = new int[no_of_resources];
+
+	for (int i = 0; i < no_of_processes; i++)
+	{
+		for (int j = 0; j < no_of_resources; j++)
+		{
+			a[i][j] = 0;
+		}
+	}
+
+	return a;
+}
+
+int** maxlist_generation(int no_of_resources, int no_of_processes, int* availblity)
+{
+	int** a = new int*[2*no_of_processes];
+	for (int i = 0; i < 2*no_of_processes; ++i)
+		a[i] = new int[no_of_resources];
+
+	for (int i = 0; i < 2*no_of_processes; i++)
+	{
+		for (int j = 0; j < no_of_resources; j++)
+		{
+			a[i][j] = std::rand() % (availblity[j]*1/3)+1;
+		}
+	}
+	return a;
+}
+
+int** requestlist_generation(int no_of_resources, int no_of_processes, int** max_list)
+{
+	int** a = new int*[2 * no_of_processes];
+	for (int i = 0; i < 2 * no_of_processes; ++i)
+		a[i] = new int[no_of_resources];
+
+	for (int i = 0; i < 2 * no_of_processes; i++)
+	{
+		for (int j = 0; j < no_of_resources; j++)
+		{
+			a[i][j] = std::rand() % max_list[i][j];
+		}
+	}
+	return a;
+}
+
+int** needlist_generation(int no_of_resources, int no_of_processes, int** max_list, int** request_list)
+{
+	int** a = new int*[2 * no_of_processes];
+	for (int i = 0; i < 2 * no_of_processes; ++i)
+		a[i] = new int[no_of_resources];
+
+	for (int i = 0; i < 2 * no_of_processes; i++)
+	{
+		for (int j = 0; j < no_of_resources; j++)
+		{
+			a[i][j] = max_list[i][j]-request_list[i][j];
+		}
+	}
+	return a;
+}
--- Banker's algorithm/main.cpp
+++ Banker's algorithm/main.cpp
@@ -17,10 +17,49 @@ int main()
 
 	#pragma region PHASE2
 	//intializing allocation matrix,max matrix,request matrix & need matrix
+	std::cout << "allocation_matrix:" << std::endl;
 	int** allocation_matrix = allocation_generation(no_of_resources, no_of_processes);
-	//int** max_matrix = maxlist_generation(no_of_resources, no_of_processes,availblity_matrix);
-	//int** request_matrix = requestlist_generation(no_of_resources, no_of_processes,max_matrix);
-	//int** need_matrix = needlist_generation(no_of_resources, no_of_processes,max_matrix,request_matrix);
+	for (int i = 0; i < no_of_processes; i++)
+	{
+		for (int j = 0; j < no_of_resources; j++)
+		{
+			std::cout << allocation_matrix[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+
+	std::cout << "max_matrix:" << std::endl;
+	int** max_matrix = maxlist_generation(no_of_resources, no_of_processes,availblity_matrix);
+	for (int i = 0; i < 2*no_of_processes; i++)
+	{
+		for (int j = 0; j < no_of_resources; j++)
+		{
+			std::cout << max_matrix[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+
+	std::cout << "request_matrix:" << std::endl;
+	int** request_matrix = requestlist_generation(no_of_resources, no_of_processes,max_matrix);
+	for (int i = 0; i < 2 * no_of_processes; i++)
+	{
+		for (int j = 0; j < no_of_resources; j++)
+		{
+			std::cout << request_matrix[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+
+	std::cout << "need_matrix:" << std::endl;
+	int** need_matrix = needlist_generation(no_of_resources, no_of_processes,max_matrix,request_matrix);
+	for (int i = 0; i < 2 * no_of_processes; i++)
+	{
+		for (int j = 0; j < no_of_resources; j++)
+		{
+			std::cout << need_matrix[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
 	#pragma endregion
 
 	system("pause");
