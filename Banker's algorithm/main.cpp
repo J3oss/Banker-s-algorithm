79350c0f68e6dba7f3064f51639bc889cbc2fc9a
Youssef Assem <34811325+J3ossOfWar@users.noreply.github.com>
2018-12-03 00:19:00 +0200

    final code adjustments

--- Banker's algorithm/main.cpp
+++ Banker's algorithm/main.cpp
@@ -1,74 +1,182 @@
 #include "iostream"
 
-#pragma region declarations
-int** copy_matrix(int row, int column, int** matrix_to_copy);
-int* copy_array(int column, int* array_to_copy);
-void  print_all(int no_of_resources, int no_of_processes, int* available_array, int** max_matrix, int** allocation_matrix, int** need_matrix);
-int** allocation_matrix_generation(int no_of_resources, int no_of_processes);
-int** need_matrix_generation(int no_of_resources, int no_of_processes);
-int** max_generation(int no_of_resources, int no_of_processes, int* availblity);
-void print_matrix(const char* messange, int** matrix_to_print, int no_of_processes, int no_of_resources);
-void print_array(const char* messange, int* array_to_print, int no_of_resources);
-int* request_generator(int no_of_resources, int mapping_number, int** max_matrix);
-void allocate(int mapping_number, int no_of_processes, int no_of_resources, int* request, int** allocation_matrix, int** max_matrix, int** need_matrix, int* availblity_matrix);
-bool test(bool& kk, int mapping_number, int no_of_processes, int no_of_resources, int** allocation_matrix, int**max_matrix, int* availble, int* request, int** need_matrix);
-#pragma endregion
+int** max_generation(int no_of_resources, int no_of_processes, int** availblity) //defines 2D array and fill it with random numbers
+{
+	int** a = new int*[no_of_processes];
+	for (int i = 0; i < no_of_processes; ++i)
+	{
+		a[i] = new int[no_of_resources];
+		for (int j = 0; j < no_of_resources; j++)
+			a[i][j] = std::rand() % (availblity[0][j]) + 1;
+	}
+	return a;
+}
+int** matrix_generation(int no_of_resources, int no_of_processes) //defines 2D array and fill it with zeros
+{
+	int** a = new int*[no_of_processes];
+	for (int i = 0; i < no_of_processes; ++i)
+	{
+		a[i] = new int[no_of_resources];
+		for (int j = 0; j < no_of_resources; j++)
+			a[i][j] = 0;
+	}
+	return a;
+}
+int** copy_matrix(int row, int column, int** matrix_to_copy)
+{
+	int** a = new int*[row];
+	for (int i = 0; i < row; ++i)
+		a[i] = new int[column];
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < column; j++)
+			a[i][j] = matrix_to_copy[i][j];
+	}
+	return a;
+}
+void  print_matrix(const char* messange, int** matrix_to_print, int no_of_processes, int no_of_resources)
+{
+	std::cout << messange << std::endl;
+	int** a = matrix_to_print;
+	for (int i = 0; i < no_of_processes; i++)
+	{
+		std::cout << "| ";
+		for (int j = 0; j < no_of_resources; j++)
+			std::cout << a[i][j] << " ";
+		std::cout << "|" << std::endl;
+	}
+}
+void  print_all(int no_of_resources, int no_of_processes, int** available_array, int** max_matrix, int** allocation_matrix, int** need_matrix)
+{
+	print_matrix("availble:", available_array, 1, no_of_resources);
+	print_matrix("max:", max_matrix, no_of_processes, no_of_resources);
+	print_matrix("allocation:", allocation_matrix, no_of_processes, no_of_resources);
+	print_matrix("need:", need_matrix, no_of_processes, no_of_resources);
+}
+
+void allocate(int mapping_number, int no_of_processes, int no_of_resources, int** request, int** allocation_matrix, int** max_matrix, int** need_matrix, int** availblity_matrix)
+{
+	for (int i = 0; i < no_of_processes; i++)
+	{
+		for (int j = 0; j < no_of_resources; j++)
+		{
+			allocation_matrix[mapping_number][j] = request[0][j];
+			need_matrix[i][j] = max_matrix[i][j] - allocation_matrix[i][j];
+			availblity_matrix[0][j] = availblity_matrix[0][j] - allocation_matrix[i][j];
+		}
+	}
+}
+bool test(int mapping_number, int no_of_processes, int no_of_resources, int** allocation_matrix, int**max_matrix, int** availble, int** request, int** need_matrix)
+{
+	for (int i = 0; i < no_of_resources; i++)
+	{
+		if (availble[0][i]<0)
+		{
+			std::cout << "system doesn't contain free resources" << std::endl;
+			std::cout << "Request is rejected" << std::endl;
+			return false;
+		}
+	}
+
+	int safe = 0;
+	int counter = 1;
+	int** finish = matrix_generation(no_of_resources,1);
+
+	for (int i = 0; i < no_of_processes; i++)//worst case scenario
+	{
+		for (int i = 0; i < no_of_processes; i++)
+		{
+			if (finish[0][i] == 0) //if finish is false
+			{
+				for (int j = 0; j < no_of_resources; j++)
+				{
+					if (need_matrix[i][j] <= availble[0][j])
+						safe++;
+				}
+
+				if (safe == no_of_resources)// if allocation of process is less than availble
+				{
+					for (int j = 0; j < no_of_resources; j++)
+						availble[0][j] = availble[0][j] + allocation_matrix[i][j];
+					finish[0][i] = counter;
+					counter++;
+					safe = 0;
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < no_of_processes; i++)
+	{
+		if (finish[i] == 0)
+		{
+			std::cout << "Request makes system unsafe" << std::endl;
+			std::cout << "Request is rejected" << std::endl;
+			return false;
+		}
+	}
+
+	std::cout << "Request leaves system safe" << std::endl;
+	std::cout << "Request is accepted and added" << std::endl;
+	return true;
+}
 
 int main()
 {
-	#pragma region PHASE1
-	int no_of_processes = 0;
-	int no_of_resources = 0;
-	int* available_array;
+	int   no_of_processes = 0; //user defined , represents number of processes in the system
+	int   no_of_resources = 0; //user defined , represents number of resources in the system
+	int   mapping_number = 0;  //user defined , which represent what process is requested
+	int** available_array;     //user defined , represents amount of each resources in the system
 
-	std::cout << "Please entre the following:" << std::endl;
+	std::cout << "Please entre the following:" << std::endl; //User inputs
 	std::cout << "1) Number of resources:";
-	std::cin >> no_of_resources;
+	std::cin  >> no_of_resources;
 	std::cout << "2) Number of processes:";
-	std::cin >> no_of_processes;
-
-	available_array = new int[no_of_resources];
-	std::cout << "Please entre the available resources:" << std::endl;
+	std::cin  >> no_of_processes;
+	available_array = new int*[no_of_resources];
+	for (int i = 0; i < 1; ++i)
+		available_array[i] = new int[no_of_resources];
 	for (int i = 0; i < no_of_resources; i++)
-	{
-		std::cin >> available_array[i];
-	}
+		std::cin >> available_array[0][i];
 	std::cout << std::endl;
 
-	int** allocation_matrix = allocation_matrix_generation(no_of_resources, no_of_processes);
+	int** allocation_matrix = matrix_generation(no_of_resources, no_of_processes); //Define all arrays
+	int** need_matrix = matrix_generation(no_of_resources, no_of_processes);
 	int** max_matrix = max_generation(no_of_resources, no_of_processes, available_array);
-	int** need_matrix = need_matrix_generation(no_of_resources, no_of_processes);
-	#pragma endregion
-
-	print_all(no_of_resources,no_of_processes,available_array,max_matrix,allocation_matrix,need_matrix);
-
-	int mapping_number = 0;//maps user input of which process requests
-	bool kk = false;//true means safe,false means unsafe
+	int** availble_from_start = copy_matrix(1,no_of_resources, available_array);
+	
+	print_all(no_of_resources, no_of_processes, available_array, max_matrix, allocation_matrix, need_matrix);
 
 	while (true)
 	{
-		std::cin >> mapping_number;
+		std::cin >> mapping_number; //User command
 
-		int* request = request_generator(no_of_resources, mapping_number, max_matrix);
-		print_array("request:", request, no_of_resources);
+		int** request = new int*[no_of_resources]; //Generate user request and print it
+		for (int i = 0; i < 1; ++i)
+			request[i] = new int[no_of_resources];
+		for (int i = 0; i < no_of_resources; i++)
+			request[0][i] = std::rand() % (max_matrix[mapping_number][i] + 1) + 0;
+		print_matrix("Request:", request, 1, no_of_resources);
 		std::cout << std::endl;
 
-		int** copy_allocation = copy_matrix(no_of_processes, no_of_resources, allocation_matrix);
+		int** copy_allocation = copy_matrix(no_of_processes, no_of_resources, allocation_matrix);//Make copies
 		int** copy_need = copy_matrix(no_of_processes, no_of_resources, need_matrix);
-		int* copy_availble = copy_array(no_of_resources, available_array);
+		int** copy_availble = copy_matrix(1, no_of_resources, available_array);
 
-		allocate(mapping_number, no_of_processes, no_of_resources, request, copy_allocation, max_matrix, copy_need, copy_availble);
+		allocate(mapping_number, no_of_processes, no_of_resources, request, copy_allocation, max_matrix, copy_need, copy_availble);//allocate in copies
 
-		
-		if (!test(kk, mapping_number, no_of_processes, no_of_resources, copy_allocation, max_matrix, copy_availble, request, copy_need))
+		if (!test(mapping_number, no_of_processes, no_of_resources, copy_allocation, max_matrix, copy_availble, request, copy_need))//result of test
 		{
-			request[0] = 0;
-			request[1] = 0;
+			for (int i = 0; i < no_of_resources; i++) //if test determines that request makes system unsafe,system deletes the request
+				request[0][i] = 0;
 		}
-		allocate(mapping_number, no_of_processes, no_of_resources, request, allocation_matrix, max_matrix, need_matrix, available_array);
+
+		allocate(mapping_number, no_of_processes, no_of_resources, request, allocation_matrix, max_matrix, need_matrix, available_array);//allocate in real
+
 		print_all(no_of_resources, no_of_processes, available_array, max_matrix, allocation_matrix, need_matrix);
-		available_array[0] = 10;
-		available_array [1] = 10;
+		
+		available_array = copy_matrix(1, no_of_resources, availble_from_start);
 	}
+
 	system("pause");
 }
